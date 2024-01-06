fe53b1ddacca506163a12e740706283566dd16e2
Pi Fisher <pif@google.com>
2024-01-06 23:35:27 +0000

    feat: Use complete dex for production calculator
    Also alphabetize list, and update names of event mons to match game.

--- backend/src/utils/pokemon-utils/pokemon-utils.ts
+++ backend/src/utils/pokemon-utils/pokemon-utils.ts
@@ -1,8 +1,8 @@
-import { OPTIMAL_POKEDEX, Pokemon } from '../../domain/pokemon/pokemon';
+import { COMPLETE_POKEDEX, OPTIMAL_POKEDEX, Pokemon } from '../../domain/pokemon/pokemon';
 import { getBerriesForFilter } from '../berry-utils/berry-utils';
 
 export function getPokemonForName(name: string): Pokemon {
-  const pokemon = OPTIMAL_POKEDEX.find((pokemon) => pokemon.name === name);
+  const pokemon = COMPLETE_POKEDEX.find((pokemon) => pokemon.name === name);
   if (!pokemon) {
     throw new Error(`Programmer error: Can't find Pokemon coming from DB with name ${name}`);
   }
