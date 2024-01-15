1a13c40f067d75789b7fdc95bbd31c2e50bcbc74
AdeosunDeji <adeosundeji2@gmail.com>
2024-01-15 20:25:51 +0100

    feat: implementing custom errors

--- backend/src/utils/ingredient-utils/ingredient-utils.ts
+++ backend/src/utils/ingredient-utils/ingredient-utils.ts
@@ -1,9 +1,10 @@
+import { IngredientError } from '../../domain/error/ingredient/ingredient-error';
 import { INGREDIENTS, Ingredient } from '../../domain/produce/ingredient';
 
 export function getIngredientForname(name: string): Ingredient {
   const ingredient = INGREDIENTS.find((ing) => ing.name.toLowerCase() === name.toLowerCase());
   if (!ingredient) {
-    throw new Error(`Ingredient with name [${name}] does not exist`);
+    throw new IngredientError(`Ingredient with name [${name}] does not exist`);
   }
   return ingredient;
 }
--- backend/src/utils/pokemon-utils/pokemon-utils.ts
+++ backend/src/utils/pokemon-utils/pokemon-utils.ts
@@ -1,10 +1,11 @@
+import { ProgrammingError } from '../../domain/error/programming/programming-error';
 import { OPTIMAL_POKEDEX, Pokemon } from '../../domain/pokemon/pokemon';
 import { getBerriesForFilter } from '../berry-utils/berry-utils';
 
 export function getPokemonForName(name: string): Pokemon {
   const pokemon = OPTIMAL_POKEDEX.find((pokemon) => pokemon.name === name);
   if (!pokemon) {
-    throw new Error(`Programmer error: Can't find Pokemon coming from DB with name ${name}`);
+    throw new ProgrammingError(`Can't find Pokemon coming from DB with name ${name}`);
   }
   return pokemon;
 }
