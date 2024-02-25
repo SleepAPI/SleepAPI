197a6722e7fa96ea6ad9c28e0435ede7c231bb85
McMomo <dev.sleepapi@gmail.com>
2024-02-25 18:53:53 +0100

    feat: added sneasel icon embedded in share svg

--- backend/src/controllers/share/share.controller.ts
+++ backend/src/controllers/share/share.controller.ts
@@ -1,5 +1,7 @@
 import { ShareParams } from '@src/routes/share-router/share-router';
 import { Request as Protocol } from 'express';
+import { readFileSync } from 'fs';
+import path from 'path';
 import sharp from 'sharp';
 import { Controller, Get, Hidden, Queries, Request, Route } from 'tsoa';
 
@@ -13,8 +15,6 @@ export default class ShareController extends Controller {
   ) {
     const pkmn = queryParams.pokemon;
     const title = `See my ${pkmn} calculation on Sleep API!`;
-    // const description = pkmn; // TODO: maybe not use?
-
     const protocol = request.protocol;
     const host = request.get('host');
     const baseUrl = `${protocol}://${host}`;
@@ -31,7 +31,9 @@ export default class ShareController extends Controller {
         <meta property="og:image" content="${imageUrl}">
         <meta property="og:image:width" content="1200">
         <meta property="og:image:height" content="600">
+        <meta property="og:image:alt" content="Pokémon Sleep production calculator result">
         <meta property="og:url" content="${this.#getFullUrl(request)}">
+        <meta property="og:type" content="website">
         <meta name="twitter:card" content="summary_large_image">
         <meta name="twitter:title" content="${title}">
         <meta name="twitter:image" content="${imageUrl}">
@@ -53,30 +55,26 @@ export default class ShareController extends Controller {
   @Get('/')
   public async getImage(@Queries() queryParams: ShareParams) {
     const { pokemon } = queryParams;
+    const pokemonCapitalized = `${pokemon[0].toUpperCase()}${pokemon.toLowerCase().slice(1)}`;
 
     const width = 1200;
     const height = 600;
+
+    const sneaselIcon = readFileSync(path.join(__dirname, '../../assets/cook.png')).toString('base64');
+
     const svgImage = `
-    <svg width="${width}" height="${height}">
-        <style>
-            .title { fill: #ddd; font-size: 24px; font-family: Arial; }
-            .desc { fill: #ddd; font-size: 20px; font-family: Arial; }
-        </style>
-        <rect width="100%" height="100%" fill="#555"/>
-        <text x="10" y="40" class="title">${pokemon}</text>
-        <text x="10" y="80" class="desc">${pokemon}</text>
+    <svg width="${width}" height="${height}" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+      <style>
+        .title { fill: #FFF; font-size: 96px; font-family: Arial; }
+        .desc { fill: #FFF; font-size: 20px; font-family: Arial; }
+      </style>
+      <rect width="100%" height="95%" fill="#555"/>
+      <rect y="95%" width="100%" height="5%" fill="#f04545"/>
+      <text x="200" y="150" class="title">${pokemonCapitalized}</text>
+      <text x="200" y="200" class="desc">${pokemon}</text>
+      <image x="1050" y="500" width="50" height="50" xlink:href="data:image/png;base64,${sneaselIcon}"/>
     </svg>
     `;
-
-    // if smaller images are needed for loading time
-    //alt 1
-    // sharp(buffer)
-    // .jpeg({ quality: 80 }) // Reducing the quality to 80%
-    // .toBuffer();
-    //alt 2
-    // sharp(buffer)
-    // .png({ quality: 80, palette: true })
-    // .toBuffer();
 
     return await sharp(Buffer.from(svgImage)).toFormat('png').toBuffer();
   }
--- backend/src/routes/share-router/share-router.ts
+++ backend/src/routes/share-router/share-router.ts
@@ -3,8 +3,23 @@ import { Request, Response } from 'express';
 import { Logger } from '../../services/logger/logger';
 import { BaseRouter } from '../base-router';
 
+// pinsir H/H/A, 2.3H, 7.3A, Skill trigger M, ing find M, help speed M, rash nature, 0 e4e, 0 erb, camp false, 3.9 skill procs
+// /share?calc=PINH2X3H0A7X3SMIMHMRAE4E0ERB0C0S3X9
 export interface ShareParams {
+  // pokemon
   pokemon: string;
+  ingredientSet: string; // apple,corn
+  // input
+  subskills: string; // CSV
+  level: number;
+  nature: string;
+  e4e?: number;
+  helpingBonus?: number;
+  erb?: number;
+  camp?: boolean;
+  // output
+  skillProcs: number;
+  producedIngredients: string; // 2,apple/3,corn // TODO: needs to support ing magnet
 }
 
 class ShareRouterImpl {
