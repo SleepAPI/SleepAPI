4bac23efcf4c774c8bdb85bc60051a0f0b2086de
McMomo <dev.sleepapi@gmail.com>
2024-08-22 21:50:51 +0200

    style: updated background image

---
+++ frontend/src/components/background/background.test.ts
@@ -0,0 +1,24 @@
+import TheBackground from '@/components/background/background.vue'
+import { VueWrapper, mount } from '@vue/test-utils'
+import { afterEach, beforeEach, describe, expect, it } from 'vitest'
+
+describe('TheBackground', () => {
+  let wrapper: VueWrapper<InstanceType<typeof TheBackground>>
+
+  beforeEach(() => {
+    wrapper = mount(TheBackground)
+  })
+
+  afterEach(() => {
+    if (wrapper) {
+      wrapper.unmount()
+    }
+  })
+
+  it('renders the background with correct attributes and styles', async () => {
+    const img = wrapper.find('img')
+
+    expect(img.exists()).toBe(true)
+    expect(img.attributes('src')).toBe('/images/site/background.png')
+  })
+})
--- frontend/src/components/background/background.vue
+++ frontend/src/components/background/background.vue
@@ -1,8 +1,5 @@
 <template>
-  <div class="blur">
-    <div class="triangle-top"></div>
-    <div class="triangle-bottom"></div>
-  </div>
+  <v-img class="background-image" src="/images/site/background.png" cover fluid></v-img>
 </template>
 
 <script lang="ts">
@@ -13,36 +10,13 @@ export default defineComponent({
 })
 </script>
 
-<style lang="scss">
-.triangle-top {
-  position: absolute;
+<style scoped>
+.background-image {
+  position: fixed;
   top: 0;
   left: 0;
-  width: 30vw;
-  height: 50vh;
-  background: rgba(230, 57, 70, 0.22);
-  clip-path: polygon(0% 0%, 0% 100%, 100% 0%);
-}
-.triangle-bottom {
-  position: absolute;
-  bottom: 0;
-  right: 0;
-  width: 30vw;
-  height: 50vh;
-  background: rgba(230, 57, 70, 0.22);
-  clip-path: polygon(0% 100%, 100% 100%, 100% 0%);
-}
-
-.blur {
-  background: rgba(255, 255, 255, 0.02);
-  height: 100dvh;
   width: 100%;
-  position: absolute;
-  filter: blur(128px);
-  -webkit-filter: blur(128px);
-  -moz-filter: blur(128px);
-  -o-filter: blur(128px);
-  -ms-filter: blur(128px);
-  overflow: hidden;
+  height: 100%;
+  z-index: -1;
 }
 </style>
