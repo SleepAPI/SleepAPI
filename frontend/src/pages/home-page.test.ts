23d96a74d4177835a859dd13230f03ac06a854ed
McMomo <dev.sleepapi@gmail.com>
2024-10-23 08:50:00 +0200

    style: desktop home page layout and update favicon

--- frontend/src/pages/home-page.test.ts
+++ frontend/src/pages/home-page.test.ts
@@ -8,16 +8,14 @@ describe('HomePage.vue', () => {
     const wrapper = mount(HomePage)
 
     const title = wrapper.find('h1.title')
-    expect(title.text()).toBe('Pokémon Sleep Simulations')
+    expect(title.text()).toBe("Neroli's Lab")
   })
 
   it('should contain the correct description text', () => {
     const wrapper = mount(HomePage)
 
     const description = wrapper.find('p')
-    expect(description.text()).toBe(
-      "Run your own simulation-based calculations with Sleep API's built-in data analysis."
-    )
+    expect(description.text()).toBe('Helping you overthink sleep tracking.')
   })
 
   it('should contain the correct feature titles and descriptions', () => {
@@ -29,7 +27,8 @@ describe('HomePage.vue', () => {
     const expectedFeatures = [
       {
         title: 'Calculator',
-        description: "Simulate your Pokémon's production with in-depth data analysis."
+        description:
+          "Calculate your team's or pokemon's production with realistic our Sleep API-powered simulations."
       },
       {
         title: 'Compare',
@@ -38,10 +37,6 @@ describe('HomePage.vue', () => {
       {
         title: 'Tier lists',
         description: 'Cooking tier lists based on millions of simulated recipe solutions.'
-      },
-      {
-        title: 'Team finder',
-        description: 'Find the most optimal teams for any given recipe.'
       }
     ]
 
--- frontend/src/pages/home-page.vue
+++ frontend/src/pages/home-page.vue
@@ -1,35 +1,31 @@
 <template>
-  <!-- TODO: Use screenshots from in game / site instead of anime screenshots for the tool images below -->
-  <v-container class="pt-12">
-    <v-row class="flex-center">
+  <v-container class="pt-12 flex-center">
+    <v-row v-if="isMobile" class="flex-center">
       <v-col cols="auto">
         <SneaselHomeIcon />
       </v-col>
-      <v-col cols="auto" class="text-center text-lg-start">
-        <h1 class="title text-h4 font-weight-medium mb-3">Pokémon Sleep Simulations</h1>
-        <p class="mb-6 mx-auto text-center">
-          Run your own simulation-based calculations with Sleep API's built-in data analysis.
-        </p>
+      <v-col cols="auto" class="text-start text-lg-start mb-8">
+        <h1 class="title text-h3 font-weight-bold mb-3">Neroli's Lab</h1>
+        <p class="mx-auto text-center">Helping you overthink sleep tracking.</p>
+        <p class="mb-6 mx-auto text-center">Optimize your strategies with our simulations.</p>
 
-        <v-row class="align-center justify-center">
-          <v-col cols="auto" class="align-center justify-center px-1">
+        <v-row class="flex-center">
+          <v-col cols="10" class="flex-center">
             <v-btn
-              class="responsive-button"
+              class="w-100 fx01"
+              title="Get started"
+              height="50px"
               size="large"
               rounded="lg"
-              color="primary"
+              aria-label="Get started"
               :to="'/calculator'"
               >Get started</v-btn
             >
           </v-col>
         </v-row>
       </v-col>
-    </v-row>
-  </v-container>
 
-  <v-container>
-    <v-row justify="center">
-      <v-col v-for="(feature, i) in features" :key="i" class="d-flex flex-shrink-1" cols="auto">
+      <v-col v-for="(feature, i) in features" :key="i" cols="auto">
         <v-card max-width="300" :to="feature.to">
           <v-img :src="feature.src" :aspect-ratio="2" width="100%" cover></v-img>
 
@@ -47,46 +43,88 @@
         </v-card>
       </v-col>
     </v-row>
+    <v-row v-else class="" style="max-width: 1000px">
+      <v-row class="justify-space-between">
+        <v-col cols="auto">
+          <h1 class="title text-h1 font-weight-bold mb-3">Neroli's Lab</h1>
+          <p class="text-h6 mx-auto text-left">Helping you overthink sleep tracking.</p>
+          <p class="text-h6 mb-6 mx-auto text-left">
+            Optimize your strategies with our simulations.
+          </p>
+          <v-btn
+            class="w-100 fx01"
+            title="Get started"
+            height="50px"
+            size="large"
+            rounded="lg"
+            aria-label="get started"
+            :to="'/calculator'"
+            >Get started</v-btn
+          >
+        </v-col>
+        <v-col cols="auto">
+          <SneaselHomeIcon />
+        </v-col>
+      </v-row>
+      <v-row class="justify-space-between flex-nowrap">
+        <v-col v-for="(feature, i) in features" :key="i" cols="auto">
+          <v-card max-width="300" class="fill-height" :to="feature.to">
+            <v-img :src="feature.src" :aspect-ratio="2" width="100%" cover></v-img>
+
+            <v-card-text>
+              <div class="d-flex">
+                <v-icon class="mr-2">{{ feature.icon }}</v-icon>
+                <h3 class="text-subheading-1 font-weight-bold mb-2">
+                  {{ feature.title }}
+                </h3>
+              </div>
+              <p class="mb-2">{{ feature.description }}</p>
+            </v-card-text>
+
+            <v-card-actions> </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-row>
   </v-container>
 </template>
 
 <script lang="ts">
 import SneaselHomeIcon from '@/components/icons/sneasel-home-icon.vue'
+import { useViewport } from '@/composables/viewport-composable'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
   components: {
     SneaselHomeIcon
   },
+  setup() {
+    const { isMobile } = useViewport()
+    return { isMobile }
+  },
   data: () => ({
     features: [
       {
-        description: "Simulate your Pokémon's production with in-depth data analysis.",
+        description:
+          "Calculate your team's or pokemon's production with realistic our Sleep API-powered simulations.",
         title: 'Calculator',
-        src: '/images/home-page/calculator.png',
+        src: '/images/misc/doctor4.png',
         icon: 'mdi-square-root',
         to: '/calculator'
       },
       {
         description: 'Compare your Pokémon to each other before deciding your investments.',
         title: 'Compare',
-        src: '/images/home-page/tierlists.png',
+        src: '/images/misc/doctor2.png',
         icon: 'mdi-compare-horizontal',
         to: '/compare'
       },
       {
         description: 'Cooking tier lists based on millions of simulated recipe solutions.',
         title: 'Tier lists',
-        src: '/images/home-page/tierlists.png',
+        src: '/images/misc/doctor3.png',
         icon: 'mdi-chart-line',
         to: '/tierlist'
-      },
-      {
-        description: 'Find the most optimal teams for any given recipe.',
-        title: 'Team finder',
-        src: '/images/home-page/teamfinder.png',
-        icon: 'mdi-text-search-variant',
-        to: '/teamfinder'
       }
     ]
   })
@@ -108,13 +146,46 @@ export default defineComponent({
   line-height: 120%;
 }
 
-.responsive-button {
-  width: 200px;
+.fx01 {
+  position: relative;
+  color: white;
+  background-color: $primary;
+  overflow: hidden;
 }
 
-@media (max-width: 450px) {
-  .responsive-button {
-    width: 150px;
+.fx01::after {
+  content: '';
+  position: absolute;
+  top: -50px;
+  left: -75px;
+  width: 50px;
+  height: 155px;
+  background: #fff;
+  opacity: 0.8;
+  transform: rotate(35deg);
+  z-index: 1;
+  animation: lightSweep 7s cubic-bezier(0.175, 0.52, 0.165, 1) infinite;
+}
+
+@keyframes lightSweep {
+  0% {
+    left: -150%;
+    opacity: 0;
   }
+  10% {
+    opacity: 0.8;
+  }
+  20% {
+    left: 120%;
+    opacity: 0;
+  }
+  100% {
+    left: 120%;
+    opacity: 0;
+  }
+}
+
+.fx01:hover {
+  background-color: $accent;
 }
 </style>
