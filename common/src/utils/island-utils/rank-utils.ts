0fa283735f271c8a2586e482977a835362ff34c0
Matt Gleason <mjgleason@foundationdevices.com>
2026-09-11 10:46:08 -0400

    feat(calculator): simplified rank calculation

--- common/src/utils/island-utils/rank-utils.ts
+++ common/src/utils/island-utils/rank-utils.ts
@@ -5,7 +5,7 @@ const RANK_NAMES = ['Basic', 'Great', 'Ultra', 'Master'];
 export function rankForStrength(strength: number, area: Area): string {
   const rankIndex = area.rankThresholds.findLastIndex((threshold) => threshold <= strength);
   const titleIndex = Math.min(Math.floor(rankIndex / 5), RANK_NAMES.length - 1);
-  const rankNumber = titleIndex < 3 ? (rankIndex % 5) + 1 : rankIndex - 14;
+  const rankNumber = rankIndex - 5 * titleIndex + 1;
 
   return `${RANK_NAMES[titleIndex]} ${rankNumber}`;
 }
